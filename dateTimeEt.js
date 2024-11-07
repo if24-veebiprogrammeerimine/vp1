de190e0ab3c26dd6f0277199b78e574fe5c59f68
Andrus Rinde <andrusrinde@users.noreply.github.com>
2024-11-07 15:31:17 +0200

    Kasutajakontode loomine lisatud

--- dateTimeEt.js
+++ dateTimeEt.js
@@ -3,10 +3,16 @@ const monthNamesEt = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni",
 
 const dateFormatted = function(){	
 	let timeNow = new Date();
+	//let specDate = new Date("12-27-1939");
 	let dateNow = timeNow.getDate();
 	let monthNow = timeNow.getMonth();
 	let yearNow = timeNow.getFullYear();
 	return dateNow + ". " + monthNamesEt[monthNow] + " " + yearNow;
+}
+
+const givenDateFormatted = function(gDate){
+	let specDate = new Date(gDate);
+	return specDate.getDate() + ". " + monthNamesEt[specDate.getMonth()] + " " + specDate.getFullYear();	
 }
 
 const weekDay = function(){
@@ -23,6 +29,24 @@ const timeFormatted = function(){
 	return hourNow + ":" + minuteNow + ":" + secondNow;
 }
 
+const daysBetween = function(gDate){
+	notice = "teadmata";
+	let today = new Date();
+	let anotherDay = new Date(gDate);
+	let diff = today - anotherDay;
+	let diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+	if(today == anotherDay){
+		notice = "täna";
+	}
+	else if(today < anotherDay){
+		notice = Math.abs(diffDays) + " päeva pärast";
+	}
+	else {
+		notice = diffDays + " päeva tagasi";
+	}
+	return notice;
+}
+
 const partOfDay = function(){
 	let dPart = "suvaline aeg";
 	let hourNow = new Date().getHours();
@@ -35,4 +59,4 @@ const partOfDay = function(){
 }
 
 //ekspordin kõik vajaliku
-module.exports = {dateFormatted: dateFormatted, weekDay: weekDay, timeFormatted: timeFormatted, weekdayNames: weekdayNamesEt, monthNames: monthNamesEt, dayPart: partOfDay};
+module.exports = {dateFormatted: dateFormatted, weekDay: weekDay, timeFormatted: timeFormatted, weekdayNames: weekdayNamesEt, monthNames: monthNamesEt, dayPart: partOfDay, givenDateFormatted: givenDateFormatted, daysBetween: daysBetween};
--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const dbInfo = require("../../../vp2024config");
 const mysql = require("mysql2");
 //päringu lahtiharutamiseks POST päringute puhul
 const bodyparser = require("body-parser");
+//failide üleslaadimiseks
+const multer = require("multer");
+//pildimanipalulatsiooniks (suuruse muutmine)
+const sharp = require("sharp");
+//parooli krüpteerimiseks
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 //päringu URL-i parsimine, false kui ainult tekst, true, kui muud ka
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({extended: true}));
+//seadistame vahevara multer fotode laadimiseks kindlasse kataloogi
+const upload = multer({dest: "./public/gallery/orig/"});
 
 //loon andmebaasiühenduse
 const connInga = mysql.createConnection({
@@ -29,7 +37,106 @@ const conn = mysql.createConnection({
 
 app.get("/", (req, res)=>{
 	//res.send("Express läks täiesti käima!");
-	res.render("index");
+	/*const fileList = fs.readdirSync("public/banner/");
+	console.log(fileList);*/
+	res.render("index",{days: dtEt.daysBetween("9-2-2024")});
+});
+
+app.get("/signin", (req, res)=>{
+	res.render("signin");
+});
+
+app.post("/signin", (req, res)=>{
+	let notice = "";
+	if(!req.body.emailInput || !req.body.passwordInput){
+		console.log("Andmeid puudu");
+		notice = "Sisselogimise andmeid on puudu!";
+		res.render("index",{notice: notice});
+	}
+	else {
+		let sqlReq = "SELECT id, password FROM vp1users WHERE email = ?";
+		conn.execute(sqlReq, [req.body.emailInput], (err, result)=>{
+			if(err){
+				console.log("Viga andmebaasist lugemisel!" + err);
+				notice = "Tehniline viga, sisselogimine ebaõnnestus!";
+				res.render("index",{notice: notice});
+			}
+			else {
+				if(result[0] != null){
+					//kasutaja on olemas, kontrollime sisestatud parooli
+					bcrypt.compare(req.body.passwordInput, result[0].password, (err, compareresult)=>{
+						if(err){
+							notice = "Tehniline viga, sisselogimine ebaõnnestus!";
+							res.render("index",{notice: notice});
+						}
+						else {
+							//kas õige või vale parool
+							if(compareresult){
+								notice = "Oled sisse loginud!";
+								res.render("index",{notice: notice});
+							}
+							else {
+								notice = "Kasutajatunnus ja/või parool on vale!";
+								res.render("index",{notice: notice});
+							}
+						}
+					});
+				}
+				else {
+					notice = "Kasutajatunnus ja/või parool on vale!";
+					res.render("index",{notice: notice});
+				}
+			}
+		});//conn.execute lõppeb
+	}
+	//res.render("index",{days: dtEt.daysBetween("9-2-2024")});
+});
+
+app.get("/signup", (req, res)=>{
+	res.render("signup");
+});
+
+app.post("/signup", (req, res)=>{
+	let notice = "Ootan andmeid!";
+	console.log(req.body);
+	if(!req.body.firstNameInput || !req.body.lastNameInput || !req.body.birthDateInput || !req.body.genderInput || !req.body.emailInput || req.body.passwordInput.length < 8 || req.body.passwordInput !== req.body.confirmPasswordInput){
+		console.log("Andmeid on puudu või paroolid ei kattu!");
+		notice = "Andmeid on puudu, parool liiga lühike või paroolid ei kattu!";
+		res.render("signup", {notice: notice});
+	}//kui andmetes viga ... lõppeb
+	else {
+		notice = "Andmed sisestatud!";
+		//loome parooli räsi jaoks "soola"
+		bcrypt.genSalt(10, (err, salt)=> {
+			if(err){
+				notice = "Tehniline viga, kasutajat ei loodud";
+				res.render("signup", {notice: notice});
+			}
+			else {
+				//krüpteerime
+				bcrypt.hash(req.body.passwordInput, salt, (err, pwdHash)=>{
+					if(err){
+						notice = "Tehniline viga parooli krüpteerimisel, kasutajat ei loodud";
+						res.render("signup", {notice: notice});
+					}
+					else {
+						let sqlReq = "INSERT INTO vp1users (first_name, last_name, birth_date, gender, email, password) VALUES(?,?,?,?,?,?)";
+						conn.execute(sqlReq, [req.body.firstNameInput, req.body.lastNameInput, req.body.birthDateInput, req.body.genderInput, req.body.emailInput, pwdHash], (err, result)=>{
+							if(err){
+								notice = "Tehniline viga andmebaasi kirjutamisel, kasutajat ei loodud.";
+								res.render("signup", {notice: notice});
+							}
+							else {
+								notice = "Kasutaja " + req.body.emailInput + " edukalt loodud!";
+								res.render("signup", {notice: notice});
+							}
+						});//conn.execute lõpp
+					}
+				});//hash lõppeb
+			}
+		});//genSalt lõppeb
+	}//kui andmed korrs, lõppeb
+	//res.render("signup");
 });
 
 app.get("/timenow", (req, res)=>{
@@ -88,6 +195,7 @@ app.post("/regvisitdb", (req, res)=>{
 	let notice = "";
 	let firstName = "";
 	let lastName = "";
+
 	if(!req.body.firstNameInput || !req.body.lastNameInput){
 		firstName = req.body.firstNameInput;
 		lastName = req.body.lastNameInput;
@@ -121,11 +229,52 @@ app.get("/eestifilm/tegelased", (req, res)=>{
 		}
 		else {
 			console.log(sqlres);
-			persons = sqlres;
+			//persons = sqlres;
+			//for    i  algab 0 piiriks sqlres.length
+			//tsükli sees lisame persons listile uue elemendi, mis on ise "objekt" {first_name: sqlres[i].first_name}
+			//listi lisamiseks on käsk    
+			//push.persons(lisatav element);
+			for (let i = 0; i < sqlres.length; i ++){
+				persons.push({first_name: sqlres[i].first_name, last_name: sqlres[i].last_name, birth_date: dtEt.givenDateFormatted(sqlres[i].birth_date)});
+			}
 			res.render("tegelased", {persons: persons});
 		}
 	});
 	//res.render("tegelased");
+});
+
+app.get("/addnews", (req, res)=>{
+	res.render("addnews");
+});
+
+app.get("/photoupload", (req, res)=>{
+	res.render("photoupload");
+});
+
+app.post("/photoupload", upload.single("photoInput"), (req, res)=>{
+	console.log(req.body);
+	console.log(req.file);
+	//genereerime oma failinime
+	const fileName = "vp_" + Date.now() + ".jpg";
+	//nimetame üleslaetud faili ümber
+	fs.rename(req.file.path, req.file.destination + fileName, (err)=>{
+		console.log(err);
+	});
+	//teeme 2 erisuurust
+	sharp(req.file.destination + fileName).resize(800,600).jpeg({quality: 90}).toFile("./public/gallery/normal/" + fileName);
+	sharp(req.file.destination + fileName).resize(100,100).jpeg({quality: 90}).toFile("./public/gallery/thumb/" + fileName);
+	//salvestame andmebaasi
+	let sqlReq = "INSERT INTO vp1photos (file_name, orig_name, alt_text, privacy, user_id) VALUES(?,?,?,?,?)";
+	const userId = 1;
+	conn.query(sqlReq, [fileName, req.file.originalname, req.body.altInput, req.body.privacyInput, userId], (err, result)=>{
+		if(err){
+			throw err;
+		}
+		else {
+			res.render("photoupload");
+		}
+	});
+	//res.render("photoupload");
 });
 
 app.listen(5100);
